7826496cc64b86db2bca7389b0bce72f23e63ef8
Fedy1507 <gayratovfirdavs720@gmail.com>
2024-04-30 12:03:40 +0500

    nothing

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os/exec"
 )
 
-const PATH = "/home/projects/go/src/NT_Homeworks/22.Working_with_git_and_remote_repositories/22-uyga-vazifa"
+const PATH = "/home/projects/go/src/NT_Homeworks/22.Working_with_git_and_remote_repositories/22-uyga-vazifa/file.txt"
 
 func PrintToFileDAta(fileName string, data string) error {
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0666)
